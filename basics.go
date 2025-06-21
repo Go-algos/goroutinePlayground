9866b59b775837d199951ca3b4b468b4a0a16bce
Pavel <pavel.oliynyk@gmail.com>
2025-06-21 13:50:51 +0100

    refactor: replace anonymous function with named function

--- basics.go
+++ basics.go
@@ -13,18 +13,20 @@ func iterate() {
 	for _, salutation := range salutations {
 		wg.Add(1)
 		// what it should be
-		go func(input *string) {
-			defer wg.Done()
-
-			id := uuid.New().String()
-			fmt.Println("Exec_id:", id)
-			fmt.Println(input)
-			var s runtime.MemStats
-			runtime.ReadMemStats(&s)
-			fmt.Println(s)
-		}(&salutation)
+		go runner(&salutation, &wg)
 	}
 
 	wg.Wait()
 	fmt.Println("All done")
+}
+
+func runner(input *string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	id := uuid.New().String()
+	fmt.Println("Exec_id:", id)
+	fmt.Println(input)
+	var s runtime.MemStats
+	runtime.ReadMemStats(&s)
+	fmt.Println(s)
 }
