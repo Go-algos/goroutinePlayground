eba6b9510f2c11656730266efb4283ba97a00796
Pavel <pavel.oliynyk@gmail.com>
2025-06-21 14:48:04 +0100

    feat: return result from goroutine via a channel

--- basics.go
+++ basics.go
@@ -8,25 +8,36 @@ import (
 )
 
 func iterate() {
+	// Create an unbuffered channel
+	c := make(chan string)
 	var wg sync.WaitGroup
 	salutations := []string{"Hello", "greetings", "Good day"}
 	for _, salutation := range salutations {
 		wg.Add(1)
 		// what it should be
-		go runner(&salutation, &wg)
-	}
+		go runner(&salutation, c, &wg)
+		// read from a channel
 
+	}
+	// read from a channel
+	for i := 0; i < len(salutations); i++ {
+		id := <-c // receive from c
+		fmt.Println("With id", id)
+	}
 	wg.Wait()
 	fmt.Println("All done")
 }
 
-func runner(input *string, wg *sync.WaitGroup) {
+func runner(input *string, c chan string, wg *sync.WaitGroup) {
+	id := uuid.New().String()
+
 	defer wg.Done()
 
-	id := uuid.New().String()
 	fmt.Println("Exec_id:", id)
 	fmt.Println(input)
 	var s runtime.MemStats
 	runtime.ReadMemStats(&s)
 	fmt.Println(s)
+	// Send back to main goroutine ID
+	c <- id
 }
